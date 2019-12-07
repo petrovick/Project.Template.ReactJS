e3667b29ed44e0a476b5f633e50d2e17ecc5cba2
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-12-06 22:29:45 -0300

    project update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import store from '../store';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:5001/api',
 });
 
 api.interceptors.request.use(config => {
